bc69f57cca06237ae78850e8068ee09a28bc1b96
John Walker <quanminhthai@outlook.com>
2025-07-13 18:52:33 +1000

    Basic embedding support

--- main.js
+++ main.js
@@ -1,16 +1,62 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu, ipcMain } = require('electron')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     minWidth: 800,
-    minHeight: 600
+    minHeight: 600,
+    webPreferences: {
+      devTools: true,
+      nodeIntegration: true,
+      javascript: true,
+      webSecurity: true,
+      images: true,
+      webgl: true,
+      scrollBounce: true,
+      contextIsolation: false,
+      webviewTag: true
+    }
   })
 
   win.loadFile('index.html')
+
+  const template = [
+    {
+      label: 'Browser',
+      submenu: [
+        {
+          label: "Devtools",
+          accelerator: 'F12',
+          click: () => {
+            win.webContents.send('devtools-open')
+          }
+        },
+      ]
+    },
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: 'Quit App',
+          accelerator: 'CommandOrControl+Q',
+          click: () => {
+            app.quit()
+          }
+        }
+      ]
+    }
+  ]
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu)
 }
 
 app.whenReady().then(() => {
-  createWindow()
+  try{
+    createWindow()
+  }catch(err){
+    console.log(err)
+  }
+  
 })
